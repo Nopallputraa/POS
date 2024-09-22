dcd3ee77c98a16c14445af7757e0dc7a80afff90
Naufal Putra Azizi Rahman <nopallpa@gmail.com>
2024-09-22 21:06:26 +0700

    jobsheet 4 prak 2.6

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,16 @@ Route::get('/', [HomeController::class, 'home']);
 
 Route::get('/', [HomeController::class, 'home']);
 
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+
+Route::put('/user/ubah_simpan/{id}',[UserController::class, 'ubah_simpan']);
+
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
 //Route::get('/user/{id}/name/{name}', [UserController::class,'user']);
 
 //Route::get('/user/{id}/name/{name}', [UserController::class,'user']);
